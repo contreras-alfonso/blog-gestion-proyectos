84c3ba915e4b1e4b786040e5e620b554eb92c5fa
Alfonso Contreras <contrerasalfonso.lino@gmail.com>
2023-10-18 01:57:06 -0500

    ec2

--- src/pages/entregables/PrimerEntregable.jsx
+++ src/pages/entregables/PrimerEntregable.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { ContenedorDoc } from '../../components/ContenedorDoc'
 import { ContenedorImg } from '../../components/ContenedorImg'
+import { Carousel } from '../../components/Carousel'
 
 export const PrimerEntregable = () => {
+
+  const images = ["/images/entregable1_1.png","/images/entregable1_2.png","/images/entregable1_3.png","/images/entregable1_4.png"]
+
   return (
     <>
     <h1 className='titulo_blog'>1. Primer entregable</h1>
@@ -14,8 +18,8 @@ export const PrimerEntregable = () => {
         <p className="subtitulo">Lógica de control - Diagrama de flujo</p>
         <ContenedorImg img={'entregable1-diagrama-de-flujo.png'} size='30'/>
         <p className="subtitulo">Ensamblaje y pruebas unitarias</p>
-        <ContenedorDoc doc={'Componentes y esquemas.pdf'}/>
-
+        <Carousel slides={images}/>
+        
         </div>
 
     </div>
