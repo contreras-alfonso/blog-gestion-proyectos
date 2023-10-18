3194b90ba9b8b3173ef264c3f246c28f4ad4825d
Alfonso Contreras <contrerasalfonso.lino@gmail.com>
2023-10-18 12:08:13 -0500

    ec2

--- src/pages/monitoreo_control/ControlDeLosCostos.jsx
+++ src/pages/monitoreo_control/ControlDeLosCostos.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ContenedorDoc } from '../../components/ContenedorDoc'
 
 export const ControlDeLosCostos = () => {
   return (
@@ -7,6 +8,12 @@ export const ControlDeLosCostos = () => {
     <div className="contenedor_blog">
         <div className="contenedor_informacion_2">
             <p className="titulo">Entradas</p>
+            <p className="subtitulo">Plan para la dirección de proyectos</p>
+            <ContenedorDoc doc='plan para la dirección de proyectos.pdf'/>
+            <p className="subtitulo">Plan para la gestión de costos</p>
+            <ContenedorDoc doc='Plan-para-la-Gestión-de-Costo.pdf'/>
+            <p className="subtitulo">Línea base de costos</p>
+            <ContenedorDoc doc='Salida- Linea base de costos.pdf'/>
             <p className="titulo">Herramientas</p>
             <p className="titulo">Salidas</p>
         </div>
