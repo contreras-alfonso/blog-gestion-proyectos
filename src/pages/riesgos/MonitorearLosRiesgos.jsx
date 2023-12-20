74819241f085c6f3ac9de71b1e9da574dcb1ff47
Alfonso Contreras <contrerasalfonso.lino@gmail.com>
2023-12-20 16:42:50 -0500

    sem16

--- src/pages/riesgos/MonitorearLosRiesgos.jsx
+++ src/pages/riesgos/MonitorearLosRiesgos.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { ContenedorImg } from '../../components/ContenedorImg'
+import { ContenedorDoc } from '../../components/ContenedorDoc'
 
 export const MonitorearLosRiesgos = () => {
   return (
@@ -13,8 +15,13 @@ export const MonitorearLosRiesgos = () => {
           <p className="subtitulo">Documentos del proyecto</p>
           <ContenedorDoc doc='Salida - Registro de riesgos.pdf' />
           <p className="titulo">Herramientas</p>
+          <p className="subtitulo">Análisis de datos</p>
+          <ContenedorImg img={'planificar_la_gestion_de_riesgos_3.jpeg'}/>
           <p className="titulo">Salidas</p>
-
+          <p className="subtitulo">Información de desempeño de trabajo</p>
+          <ContenedorDoc doc='Información de desempeño de trabajo.pdf' />
+          <p className="subtitulo">Actualización del plan de dirección de proyecto</p>
+          <ContenedorDoc doc='plan para la dirección de proyectos.pdf' />
 
         </div>
       </div>
